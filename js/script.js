9922e08aa4b71274a4ee19b718487aba1d21fc34
Ashad <ashadnasim72@gmail.com>
2020-11-24 15:04:51 +0530

    all are tested

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ $(document).ready(function () {
 		centerPadding: '60px',
 		autoplay: true,
 		arrows: false,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 800,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
 	});
 });
 $(document).ready(function () {
