dd71ff1b12ca5a83e6b075fe7d60b5031e4dc5c0
Ashad <ashadnasim72@gmail.com>
2020-11-24 14:47:21 +0530

    done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ $(document).ready(function () {
 		arrows: false,
 	});
 });
+$(document).ready(function () {
+	$('.top').slick({
+		dots: false,
+		infinite: true,
+		speed: 200,
+		slidesToShow: 1,
+		autoplay: true,
+		arrows: true,
+		cssEase: 'linear',
+	});
+});
+
+$('.youtuber-left ').click(function () {
+	$('.slider').slick('slickPrev');
+});
+
+$('.youtuber-right').click(function () {
+	$('.slider').slick('slickNext');
+});
+
+$('.team-left ').click(function () {
+	$('.slider').slick('slickPrev');
+});
+
+$('.team-right').click(function () {
+	$('.slider').slick('slickNext');
+});
